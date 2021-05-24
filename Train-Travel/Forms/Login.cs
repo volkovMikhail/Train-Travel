fd9c96a9bf1f9d7368b8b7968ff34cd1dd2f1f46
Mikhail <volkov.work1@gmail.com>
2021-05-24 15:04:59 +0300

    initial

---
+++ Train-Travel/Forms/Login.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Train_Travel.Forms;
+
+namespace Train_Travel.Forms
+{
+    public partial class Login : Form
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            this.Hide();
+            Registration reg = new Registration();
+            reg.ShowDialog();
+            this.Show();
+        }
+    }
+}
