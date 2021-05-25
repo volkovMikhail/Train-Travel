aea4cbdb7286b82aa6dbfd3fd18d9517d141a4b6
Mikhail Volkov <volkov.work1@gmail.com>
2021-05-25 23:33:35 +0300

    create mainUser form interface

--- Train-Travel/Forms/MainUser.Designer.cs
+++ Train-Travel/Forms/MainUser.Designer.cs
@@ -31,22 +31,32 @@ namespace Train_Travel
         {
             this.tabControl1 = new System.Windows.Forms.TabControl();
             this.tabPage1 = new System.Windows.Forms.TabPage();
-            this.tabPage2 = new System.Windows.Forms.TabPage();
             this.splitContainer1 = new System.Windows.Forms.SplitContainer();
             this.listViewVoyages = new System.Windows.Forms.ListView();
             this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader7 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.columnHeader5 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.columnHeader6 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
-            this.columnHeader7 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox4 = new System.Windows.Forms.GroupBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBoxEndPrice = new System.Windows.Forms.TextBox();
+            this.textBoxStartPrice = new System.Windows.Forms.TextBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dateTimePickerStartDate = new System.Windows.Forms.DateTimePicker();
+            this.dateTimePickerEndDate = new System.Windows.Forms.DateTimePicker();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
             this.comboBox1 = new System.Windows.Forms.ComboBox();
             this.comboBox2 = new System.Windows.Forms.ComboBox();
-            this.comboBox3 = new System.Windows.Forms.ComboBox();
-            this.dateTimePicker2 = new System.Windows.Forms.DateTimePicker();
-            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
             this.tabControl1.SuspendLayout();
             this.tabPage1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
@@ -54,6 +64,9 @@ namespace Train_Travel
             this.splitContainer1.Panel2.SuspendLayout();
             this.splitContainer1.SuspendLayout();
             this.groupBox1.SuspendLayout();
+            this.groupBox4.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.groupBox2.SuspendLayout();
             this.SuspendLayout();
             // 
             // tabControl1
@@ -64,7 +77,7 @@ namespace Train_Travel
             this.tabControl1.Location = new System.Drawing.Point(0, 0);
             this.tabControl1.Name = "tabControl1";
             this.tabControl1.SelectedIndex = 0;
-            this.tabControl1.Size = new System.Drawing.Size(834, 511);
+            this.tabControl1.Size = new System.Drawing.Size(784, 533);
             this.tabControl1.TabIndex = 0;
             // 
             // tabPage1
@@ -73,20 +86,10 @@ namespace Train_Travel
             this.tabPage1.Location = new System.Drawing.Point(4, 22);
             this.tabPage1.Name = "tabPage1";
             this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage1.Size = new System.Drawing.Size(826, 485);
+            this.tabPage1.Size = new System.Drawing.Size(776, 507);
             this.tabPage1.TabIndex = 0;
             this.tabPage1.Text = "Рейсы";
             this.tabPage1.UseVisualStyleBackColor = true;
-            // 
-            // tabPage2
-            // 
-            this.tabPage2.Location = new System.Drawing.Point(4, 22);
-            this.tabPage2.Name = "tabPage2";
-            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage2.Size = new System.Drawing.Size(826, 485);
-            this.tabPage2.TabIndex = 1;
-            this.tabPage2.Text = "Личный кабинет";
-            this.tabPage2.UseVisualStyleBackColor = true;
             // 
             // splitContainer1
             // 
@@ -102,8 +105,8 @@ namespace Train_Travel
             // splitContainer1.Panel2
             // 
             this.splitContainer1.Panel2.Controls.Add(this.groupBox1);
-            this.splitContainer1.Size = new System.Drawing.Size(820, 479);
-            this.splitContainer1.SplitterDistance = 320;
+            this.splitContainer1.Size = new System.Drawing.Size(770, 501);
+            this.splitContainer1.SplitterDistance = 374;
             this.splitContainer1.TabIndex = 0;
             // 
             // listViewVoyages
@@ -122,7 +125,7 @@ namespace Train_Travel
             this.listViewVoyages.HideSelection = false;
             this.listViewVoyages.Location = new System.Drawing.Point(0, 0);
             this.listViewVoyages.Name = "listViewVoyages";
-            this.listViewVoyages.Size = new System.Drawing.Size(820, 320);
+            this.listViewVoyages.Size = new System.Drawing.Size(770, 374);
             this.listViewVoyages.TabIndex = 0;
             this.listViewVoyages.UseCompatibleStateImageBehavior = false;
             this.listViewVoyages.View = System.Windows.Forms.View.Details;
@@ -136,6 +139,11 @@ namespace Train_Travel
             // 
             this.columnHeader2.Text = "Место прибытия";
             this.columnHeader2.Width = 137;
+            // 
+            // columnHeader7
+            // 
+            this.columnHeader7.Text = "Тип рейса";
+            this.columnHeader7.Width = 107;
             // 
             // columnHeader3
             // 
@@ -157,76 +165,182 @@ namespace Train_Travel
             this.columnHeader6.Text = "Цена";
             this.columnHeader6.Width = 92;
             // 
-            // columnHeader7
-            // 
-            this.columnHeader7.Text = "Тип рейса";
-            this.columnHeader7.Width = 107;
-            // 
             // groupBox1
             // 
-            this.groupBox1.Controls.Add(this.dateTimePicker1);
-            this.groupBox1.Controls.Add(this.dateTimePicker2);
-            this.groupBox1.Controls.Add(this.comboBox3);
-            this.groupBox1.Controls.Add(this.comboBox2);
-            this.groupBox1.Controls.Add(this.comboBox1);
+            this.groupBox1.Controls.Add(this.groupBox4);
+            this.groupBox1.Controls.Add(this.groupBox3);
+            this.groupBox1.Controls.Add(this.groupBox2);
             this.groupBox1.Location = new System.Drawing.Point(3, 3);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(814, 149);
+            this.groupBox1.Size = new System.Drawing.Size(764, 117);
             this.groupBox1.TabIndex = 0;
             this.groupBox1.TabStop = false;
-            this.groupBox1.Text = "groupBox1";
-            
+            this.groupBox1.Text = "Поиск рейса";
+            // 
+            // groupBox4
+            // 
+            this.groupBox4.Controls.Add(this.label4);
+            this.groupBox4.Controls.Add(this.label3);
+            this.groupBox4.Controls.Add(this.textBoxEndPrice);
+            this.groupBox4.Controls.Add(this.textBoxStartPrice);
+            this.groupBox4.Location = new System.Drawing.Point(566, 19);
+            this.groupBox4.Name = "groupBox4";
+            this.groupBox4.Size = new System.Drawing.Size(192, 90);
+            this.groupBox4.TabIndex = 10;
+            this.groupBox4.TabStop = false;
+            this.groupBox4.Text = "Поиск по цене";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(25, 53);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(22, 13);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "До";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(25, 26);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(20, 13);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "От";
+            // 
+            // textBoxEndPrice
+            // 
+            this.textBoxEndPrice.Location = new System.Drawing.Point(60, 50);
+            this.textBoxEndPrice.Name = "textBoxEndPrice";
+            this.textBoxEndPrice.Size = new System.Drawing.Size(108, 20);
+            this.textBoxEndPrice.TabIndex = 6;
+            this.textBoxEndPrice.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBoxEndPrice_KeyPress);
+            // 
+            // textBoxStartPrice
+            // 
+            this.textBoxStartPrice.Location = new System.Drawing.Point(60, 23);
+            this.textBoxStartPrice.Name = "textBoxStartPrice";
+            this.textBoxStartPrice.Size = new System.Drawing.Size(108, 20);
+            this.textBoxStartPrice.TabIndex = 7;
+            this.textBoxStartPrice.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBoxStartPrice_KeyPress_1);
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.label2);
+            this.groupBox3.Controls.Add(this.label1);
+            this.groupBox3.Controls.Add(this.dateTimePickerStartDate);
+            this.groupBox3.Controls.Add(this.dateTimePickerEndDate);
+            this.groupBox3.Location = new System.Drawing.Point(270, 19);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(290, 90);
+            this.groupBox3.TabIndex = 9;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Поиск по времени";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(21, 53);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(92, 13);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "Время прибытия";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(21, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(108, 13);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "Время отправления";
+            // 
+            // dateTimePickerStartDate
+            // 
+            this.dateTimePickerStartDate.CustomFormat = "dd.MM.yyyy hh:mm";
+            this.dateTimePickerStartDate.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.dateTimePickerStartDate.Location = new System.Drawing.Point(149, 23);
+            this.dateTimePickerStartDate.Name = "dateTimePickerStartDate";
+            this.dateTimePickerStartDate.Size = new System.Drawing.Size(121, 20);
+            this.dateTimePickerStartDate.TabIndex = 4;
+            this.dateTimePickerStartDate.Value = new System.DateTime(2020, 1, 1, 0, 0, 0, 0);
+            // 
+            // dateTimePickerEndDate
+            // 
+            this.dateTimePickerEndDate.CustomFormat = "dd.MM.yyyy hh:mm";
+            this.dateTimePickerEndDate.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.dateTimePickerEndDate.Location = new System.Drawing.Point(149, 49);
+            this.dateTimePickerEndDate.Name = "dateTimePickerEndDate";
+            this.dateTimePickerEndDate.Size = new System.Drawing.Size(121, 20);
+            this.dateTimePickerEndDate.TabIndex = 5;
+            this.dateTimePickerEndDate.Value = new System.DateTime(2021, 5, 25, 21, 16, 20, 0);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.label6);
+            this.groupBox2.Controls.Add(this.label5);
+            this.groupBox2.Controls.Add(this.comboBox1);
+            this.groupBox2.Controls.Add(this.comboBox2);
+            this.groupBox2.Location = new System.Drawing.Point(6, 19);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(258, 90);
+            this.groupBox2.TabIndex = 8;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Поиск по месту";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(6, 53);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(91, 13);
+            this.label6.TabIndex = 4;
+            this.label6.Text = "Место прибытия";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(6, 26);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(107, 13);
+            this.label5.TabIndex = 3;
+            this.label5.Text = "Место отправления";
             // 
             // comboBox1
             // 
             this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(100, 35);
+            this.comboBox1.Location = new System.Drawing.Point(118, 23);
             this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.Size = new System.Drawing.Size(123, 21);
             this.comboBox1.TabIndex = 0;
             // 
             // comboBox2
             // 
             this.comboBox2.FormattingEnabled = true;
-            this.comboBox2.Location = new System.Drawing.Point(100, 62);
+            this.comboBox2.Location = new System.Drawing.Point(118, 50);
             this.comboBox2.Name = "comboBox2";
-            this.comboBox2.Size = new System.Drawing.Size(121, 21);
+            this.comboBox2.Size = new System.Drawing.Size(123, 21);
             this.comboBox2.TabIndex = 1;
             // 
-            // comboBox3
+            // tabPage2
             // 
-            this.comboBox3.FormattingEnabled = true;
-            this.comboBox3.Location = new System.Drawing.Point(100, 90);
-            this.comboBox3.Name = "comboBox3";
-            this.comboBox3.Size = new System.Drawing.Size(121, 21);
-            this.comboBox3.TabIndex = 2;
-            // 
-            // dateTimePicker2
-            // 
-            this.dateTimePicker2.CustomFormat = "dd.MM.yyyy hh:mm";
-            this.dateTimePicker2.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
-            this.dateTimePicker2.Location = new System.Drawing.Point(376, 36);
-            this.dateTimePicker2.Name = "dateTimePicker2";
-            this.dateTimePicker2.Size = new System.Drawing.Size(145, 20);
-            this.dateTimePicker2.TabIndex = 4;
-            // 
-            // dateTimePicker1
-            // 
-            this.dateTimePicker1.CustomFormat = "dd.MM.yyyy hh:mm";
-            this.dateTimePicker1.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
-            this.dateTimePicker1.Location = new System.Drawing.Point(376, 63);
-            this.dateTimePicker1.Name = "dateTimePicker1";
-            this.dateTimePicker1.Size = new System.Drawing.Size(145, 20);
-            this.dateTimePicker1.TabIndex = 5;
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(776, 507);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Личный кабинет";
+            this.tabPage2.UseVisualStyleBackColor = true;
             // 
             // mainUser
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(834, 511);
+            this.ClientSize = new System.Drawing.Size(784, 533);
             this.Controls.Add(this.tabControl1);
             this.Name = "mainUser";
             this.Text = "Train Travel";
+            this.Load += new System.EventHandler(this.mainUser_Load);
             this.tabControl1.ResumeLayout(false);
             this.tabPage1.ResumeLayout(false);
             this.splitContainer1.Panel1.ResumeLayout(false);
@@ -234,6 +348,12 @@ namespace Train_Travel
             ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
             this.splitContainer1.ResumeLayout(false);
             this.groupBox1.ResumeLayout(false);
+            this.groupBox4.ResumeLayout(false);
+            this.groupBox4.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -253,11 +373,21 @@ namespace Train_Travel
         private System.Windows.Forms.ColumnHeader columnHeader5;
         private System.Windows.Forms.ColumnHeader columnHeader6;
         private System.Windows.Forms.GroupBox groupBox1;
-        private System.Windows.Forms.DateTimePicker dateTimePicker2;
-        private System.Windows.Forms.ComboBox comboBox3;
+        private System.Windows.Forms.DateTimePicker dateTimePickerStartDate;
         private System.Windows.Forms.ComboBox comboBox2;
         private System.Windows.Forms.ComboBox comboBox1;
-        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.DateTimePicker dateTimePickerEndDate;
+        private System.Windows.Forms.TextBox textBoxStartPrice;
+        private System.Windows.Forms.TextBox textBoxEndPrice;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox4;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
     }
 }
 
--- Train-Travel/Forms/MainUser.cs
+++ Train-Travel/Forms/MainUser.cs
@@ -7,16 +7,39 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Train_Travel.Model;
 
 namespace Train_Travel
 {
     public partial class mainUser : Form
     {
+        User user;
         
-
         public mainUser(int userId)
         {
             InitializeComponent();
+            user = new User(userId);
+        }
+        private void mainUser_Load(object sender, EventArgs e)
+        {
+            dateTimePickerEndDate.Value = DateTime.Now.AddDays(90);
+            dateTimePickerStartDate.Value = DateTime.Now;
+        }
+
+        private void textBoxEndPrice_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!(Char.IsDigit(e.KeyChar)) && !((e.KeyChar == ',') && (textBoxEndPrice.Text.IndexOf(",") == -1) && (textBoxEndPrice.Text.Length != 0)))
+            {
+                if (e.KeyChar != (char)Keys.Back) e.Handled = true;
+            }
+        }
+
+        private void textBoxStartPrice_KeyPress_1(object sender, KeyPressEventArgs e)
+        {
+            if (!(Char.IsDigit(e.KeyChar)) && !((e.KeyChar == ',') && (textBoxStartPrice.Text.IndexOf(",") == -1) && (textBoxStartPrice.Text.Length != 0)))
+            {
+                if (e.KeyChar != (char)Keys.Back) e.Handled = true;
+            }
         }
     }
 }
